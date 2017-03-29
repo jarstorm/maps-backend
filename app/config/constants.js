bee5176fca5e83947a6561cb46f6fa2ec70e3afc
Javier Abad <jabad@openkm.com>
2017-03-29 10:44:36 +0200

    heroku

--- app/config/constants.js
+++ app/config/constants.js
@@ -3,69 +3,69 @@ import merge from 'lodash/merge';
 
 // Default configuations applied to all environments
 const defaultConfig = {
-  env: process.env.NODE_ENV,
-  get envs() {
-    return {
-      test: process.env.NODE_ENV === 'test',
-      development: process.env.NODE_ENV === 'development',
-      production: process.env.NODE_ENV === 'production',
-    };
-  },
-
-  version: require('../../package.json').version,
-  root: path.normalize(__dirname + '/../../..'),
-  port: process.env.PORT || 4567,
-  ip: process.env.IP || '0.0.0.0',
-  apiPrefix: '', // Could be /api/resource or /api/v2/resource
-  userRoles: ['guest', 'user', 'admin'],
-
-  /**
-   * MongoDB configuration options
-   */
-  mongo: {
-    seed: true,
-    options: {
-      db: {
-        safe: true,
-      },
+    env: process.env.NODE_ENV,
+    get envs() {
+        return {
+            test: process.env.NODE_ENV === 'test',
+            development: process.env.NODE_ENV === 'development',
+            production: process.env.NODE_ENV === 'production',
+        };
     },
-  },
 
-  /**
-   * Security configuation options regarding sessions, authentication and hashing
-   */
-  security: {
-    sessionSecret: process.env.SESSION_SECRET || 'i-am-the-secret-key',
-    sessionExpiration: process.env.SESSION_EXPIRATION || 60 * 60 * 24 * 7, // 1 week
-    saltRounds: process.env.SALT_ROUNDS || 12,
-  },
+    version: require('../../package.json').version,
+    root: path.normalize(__dirname + '/../../..'),
+    port: process.env.PORT || 4567,
+    ip: process.env.IP || '0.0.0.0',
+    apiPrefix: '', // Could be /api/resource or /api/v2/resource
+    userRoles: ['guest', 'user', 'admin'],
+
+    /**
+     * MongoDB configuration options
+     */
+    mongo: {
+        seed: true,
+        options: {
+            db: {
+                safe: true,
+            },
+        },
+    },
+
+    /**
+     * Security configuation options regarding sessions, authentication and hashing
+     */
+    security: {
+        sessionSecret: process.env.SESSION_SECRET || 'i-am-the-secret-key',
+        sessionExpiration: process.env.SESSION_EXPIRATION || 60 * 60 * 24 * 7, // 1 week
+        saltRounds: process.env.SALT_ROUNDS || 12,
+    },
 };
 
 // Environment specific overrides
 const environmentConfigs = {
-  development: {
-    mongo: {
-      uri: process.env.MONGO_URI || 'mongodb://localhost/development',
+    development: {
+        mongo: {
+            uri: process.env.MONGO_URI || 'mongodb://localhost/development',
+        },
+        security: {
+            saltRounds: 4,
+        },
     },
-    security: {
-      saltRounds: 4,
+    test: {
+        port: 5678,
+        mongo: {
+            uri: process.env.MONGO_URI || 'mongodb://localhost/test',
+        },
+        security: {
+            saltRounds: 4,
+        },
     },
-  },
-  test: {
-    port: 5678,
-    mongo: {
-      uri: process.env.MONGO_URI || 'mongodb://localhost/test',
+    production: {
+        mongo: {
+            seed: false,
+            uri: process.env.MONGO_URI || 'mongodb://jarstorm:jarstorm@ds119220.mlab.com:19220/map',
+        },
     },
-    security: {
-      saltRounds: 4,
-    },
-  },
-  production: {
-    mongo: {
-      seed: false,
-      uri: process.env.MONGO_URI,
-    },
-  },
 };
 
 // Recursively merge configurations
--- build/config/constants.js
+++ build/config/constants.js
@@ -1,7 +1,7 @@
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-  value: true
+    value: true
 });
 
 var _path = require('path');
@@ -16,69 +16,69 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 // Default configuations applied to all environments
 var defaultConfig = {
-  env: process.env.NODE_ENV,
-  get envs() {
-    return {
-      test: process.env.NODE_ENV === 'test',
-      development: process.env.NODE_ENV === 'development',
-      production: process.env.NODE_ENV === 'production'
-    };
-  },
+    env: process.env.NODE_ENV,
+    get envs() {
+        return {
+            test: process.env.NODE_ENV === 'test',
+            development: process.env.NODE_ENV === 'development',
+            production: process.env.NODE_ENV === 'production'
+        };
+    },
 
-  version: require('../../package.json').version,
-  root: _path2.default.normalize(__dirname + '/../../..'),
-  port: process.env.PORT || 4567,
-  ip: process.env.IP || '0.0.0.0',
-  apiPrefix: '', // Could be /api/resource or /api/v2/resource
-  userRoles: ['guest', 'user', 'admin'],
+    version: require('../../package.json').version,
+    root: _path2.default.normalize(__dirname + '/../../..'),
+    port: process.env.PORT || 4567,
+    ip: process.env.IP || '0.0.0.0',
+    apiPrefix: '', // Could be /api/resource or /api/v2/resource
+    userRoles: ['guest', 'user', 'admin'],
 
-  /**
-   * MongoDB configuration options
-   */
-  mongo: {
-    seed: true,
-    options: {
-      db: {
-        safe: true
-      }
+    /**
+     * MongoDB configuration options
+     */
+    mongo: {
+        seed: true,
+        options: {
+            db: {
+                safe: true
+            }
+        }
+    },
+
+    /**
+     * Security configuation options regarding sessions, authentication and hashing
+     */
+    security: {
+        sessionSecret: process.env.SESSION_SECRET || 'i-am-the-secret-key',
+        sessionExpiration: process.env.SESSION_EXPIRATION || 60 * 60 * 24 * 7, // 1 week
+        saltRounds: process.env.SALT_ROUNDS || 12
     }
-  },
-
-  /**
-   * Security configuation options regarding sessions, authentication and hashing
-   */
-  security: {
-    sessionSecret: process.env.SESSION_SECRET || 'i-am-the-secret-key',
-    sessionExpiration: process.env.SESSION_EXPIRATION || 60 * 60 * 24 * 7, // 1 week
-    saltRounds: process.env.SALT_ROUNDS || 12
-  }
 };
 
 // Environment specific overrides
 var environmentConfigs = {
-  development: {
-    mongo: {
-      uri: process.env.MONGO_URI || 'mongodb://localhost/development'
+    development: {
+        mongo: {
+            uri: process.env.MONGO_URI || 'mongodb://localhost/development'
+        },
+        security: {
+            saltRounds: 4
+        }
     },
-    security: {
-      saltRounds: 4
-    }
-  },
-  test: {
-    port: 5678,
-    mongo: {
-      uri: process.env.MONGO_URI || 'mongodb://localhost/test'
+    test: {
+        port: 5678,
+        mongo: {
+            uri: process.env.MONGO_URI || 'mongodb://localhost/test'
+        },
+        security: {
+            saltRounds: 4
+        }
     },
-    security: {
-      saltRounds: 4
+    production: {
+        mongo: {
+            seed: false,
+            uri: process.env.MONGO_URI || 'mongodb://jarstorm:jarstorm@ds119220.mlab.com:19220/map'
+        }
     }
-  },
-  production: {
-    mongo: {
-      seed: false,
-      uri: process.env.MONGO_URI
-    }
-  }
 };
 
 // Recursively merge configurations
